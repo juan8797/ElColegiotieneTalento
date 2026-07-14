56f068fa2ce20668f06e96cbcf48d32ddbb73eb0
juan8797 <juanjoseriosgiraldo2010@gmail.com>
2026-07-14 09:37:23 -0500

    cambio inicio sesión

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <nav class="menu">
   <a href="index.php" id="menus">Odjetivo</a>
   <a href="registro.php" id="menus">Registro</a>
-  <a href="login.php" id="menus">Inicia secion</a>
+  <a href="login.php" id="menus">Inicia sesion</a>
 </nav>
